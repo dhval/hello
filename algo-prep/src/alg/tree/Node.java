021a5f4a95320ace4887a9e6c95f357c49dacdb1
Dhval Mudawal <dmudawal@lexmark.com>
2012-08-24 13:43:19 -0400

    init1

--- algo-prep/src/alg/tree/Node.java
+++ algo-prep/src/alg/tree/Node.java
@@ -2,69 +2,76 @@ package alg.tree;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-public class Node implements Comparable<Node>{
-	
-	protected char data;
-	protected Node left;
-	protected Node right;
-	protected Node rand;
-	private int size;
-	
-	public Node(char data) {
-		super();
-		this.data = data;
-		left = right = null;
-	}
+public class Node<T> implements Comparable<Node<T>> {
 
-	public Node(char[] data) {
-		super();
-		Arrays.sort(data);
-		size = data.length;
-		int mid = (data.length)/2;
-		this.data = data[mid];
-		left = createTree(data, 0, mid-1);
-		right = createTree(data, mid+1, data.length-1);
-	}
+    protected T data;
+    protected Node<T> left;
+    protected Node<T> right;
+    protected Node<T> rand;
+    private int size;
 
-	public List<Character> asList() {
-		List<Character> nodes = new ArrayList<Character>();
-		inOrder(this, nodes);
-		return nodes;
-	}
-	
-	
-	public String toString() {
-		return this.asList().toString();
-	}
-	
-	private Node createTree(char[] data, int l, int h) {
-		int mid = (h+l)/2;
-		if (mid < 0) return null;
-		Node tmp = new Node(data[mid]);
-		if (mid-l > 0) {tmp.left = createTree(data, l, mid-1);}	
-		if (h-mid > 0) {tmp.right = createTree(data, mid+1, h);}
-		return tmp;
-	}
-	
-	/**
-	 * Use InOrder Traversal to Print
-	 * @param root
-	 */
-	public void inOrder(Node root, List<Character> nodes) {	
-		if (root == null) {
-			return;
-		}
-		inOrder(root.left, nodes);
-		nodes.add(root.data);
-		inOrder(root.right, nodes);		
-	}
+    public Node(final T data) {
+	super();
+	this.data = data;
+	left = right = null;
+    }
 
-	@Override
-	public int compareTo(Node o) {
-		return data - o.data;
+    public Node(final T[] data) {
+	super();
+	Arrays.sort(data);
+	size = data.length;
+	final int mid = (data.length) / 2;
+	this.data = data[mid];
+	left = createTree(data, 0, mid - 1);
+	right = createTree(data, mid + 1, data.length - 1);
+    }
+
+    public List<T> asList() {
+	final List<T> nodes = new ArrayList<T>();
+	inOrder(this, nodes);
+	return nodes;
+    }
+
+    @Override
+    public String toString() {
+	return this.asList().toString();
+    }
+
+    private Node<T> createTree(final T[] data, final int l, final int h) {
+	final int mid = (h + l) / 2;
+	if (mid < 0)
+	    return null;
+	final Node<T> tmp = new Node<T>(data[mid]);
+	if (mid - l > 0) {
+	    tmp.left = createTree(data, l, mid - 1);
 	}
+	if (h - mid > 0) {
+	    tmp.right = createTree(data, mid + 1, h);
+	}
+	return tmp;
+    }
+
+    /**
+     * Use InOrder Traversal to Print
+     * 
+     * @param root
+     */
+    public void inOrder(final Node<T> root, final List<T> nodes) {
+	if (root == null) {
+	    return;
+	}
+	inOrder(root.left, nodes);
+	nodes.add(root.data);
+	inOrder(root.right, nodes);
+    }
+
+    @Override
+    public int compareTo(final Node<T> o) {
+	if (o instanceof Node<Character>) {
+	    return data - o.data;
+	}
+    }
 
 }
