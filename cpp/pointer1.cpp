3b06ce3ba4c1af2bf04c925ec08c9dba90d898cd
Dhval Mudawal <dmudawal@lexmark.com>
2012-09-11 11:46:54 -0400

    all

---
+++ cpp/pointer1.cpp
@@ -0,0 +1,40 @@
+
+// hello.cpp: Maggie Johnson
+// Description: a program that prints the immortal saying "hello world"
+
+#include <iostream>
+using namespace std;
+
+void Unknown(int *p, int num);
+void HardToFollow(int *p, int q, int *num);
+
+void Unknown(int *p, int num) {
+ int *q;
+
+ q = &num;
+ *p = *q + 2;
+ num = 7;
+}
+
+void HardToFollow(int *p, int q, int *num) {
+ *p = q + *num;
+ *num = q;
+ num = p;
+ p = &q;
+ Unknown(num, *p);
+}
+
+main() {
+ int *q;
+ int trouble[3];
+
+ trouble[0] = 1;
+ q = &trouble[1];
+ *q = 2;
+ trouble[2] = 3;
+
+ HardToFollow(q, trouble[0], &trouble[2]);
+ Unknown(&trouble[0], *q);
+
+ cout << *q << " " << trouble[0] << " " << trouble[2];
+}
