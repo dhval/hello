1deb4b241efb1736eaff7f90e7d221c1d16cd83c
Dhval Mudawal <d.hvalm@gmail.com>
2016-09-03 22:52:56 +0000

    new stuff

--- src/main/java/api/Regex.java
+++ src/main/java/api/Regex.java
@@ -4,44 +4,34 @@ import java.util.regex.*;
 
 public class Regex {
 
+	/**
+	 * Simple look ahead for any known key-word as delimiter, .* -> to match any string.
+	 **/
+	static String LOOK_AHEAD =  "(.*?)(BACK|CHEST|HEAD|LEFT ARM|LEFT HAND)(.*)";
 	static String NON_CAPTURING_GROUP = "my (?:cat|dog) pooped"; // [...]  is a character set
+	/**
+	 * Since \\1 represents part matched by group 1 th
+	 **/
+    static String CONSECUTIVE_NUM = "(\\d)\\1";
 
-	static Pattern patternOrg3 = Pattern.compile("^cn=((business partners|county|federal|state)\\s*(Group\\s*[\\d]+))\\s*?,*?");
+	static Pattern patternOrg3 = Pattern.compile("^cn=((business partners|county|federal|state)\\s*(Group\\s*[\\d]+)).*");
 	static Pattern patternUser = Pattern.compile("^cn=([^,]*),*");
 	static Pattern PATTERN_DOT_DT = Pattern.compile("^(\\d{4}\\.\\d{2}\\.\\d{2})$");
-	static Pattern patternG20 = Pattern.compile("^cn=((business partners|county|federal|state)-(\\w*)),ou=*");
 
-	static Pattern pattern = Pattern.compile("^t[r|s]p");
+	static Pattern TEST_PATTERN = Pattern.compile("^t[r|s]p");
 
 	public static void checkRegex(Pattern pattern, String input) {
 		Matcher matcher = pattern.matcher(input);
-		if (matcher.find()) {
-			System.out.println("Match found ... counts#" + matcher.groupCount() + " --- " + matcher.group(0));
-			for (int i = 1; i <= matcher.groupCount(); i++)
-				System.out.println("Group " + i + " --- " + matcher.group(i));
-		}
-		else
-		System.out.println("... No Match ...");
-	}
-
-	static String toDate(String input) {
-		if (input == null || input.isEmpty())
-			 return "";
-		return input.replace("-", "\\");
-	}
-
-	static String parse(String input) {
-		if (input == null || input.isEmpty())
-			 return "";
-			System.out.println(input);
-			Pattern pattern = Pattern.compile("(.*?)(BACK|CHEST|HEAD|LEFT ARM|LEFT HAND)(.*)");
-			Matcher matcher = pattern.matcher(input);
-			if (matcher.find() && matcher.groupCount()==3) {
-					System.out.println("Match found ... counts#" + matcher.groupCount() + " --- " + matcher.group(0));
-				 return matcher.group(1) + "," + matcher.group(2) + "," + matcher.group(3);
+		boolean isMatch = matcher.find();
+		do {
+			if (!isMatch) { 
+				System.out.println("... No Match ...");		
+			} else {
+				System.out.println("Match found ... counts#" + matcher.groupCount() + " --- " + matcher.group(0));
+				for (int i = 1; i <= matcher.groupCount(); i++)
+					System.out.println("Group " + i + " --- " + matcher.group(i));
 			}
-			//unable to parse
-			return input.concat(" d,,");
+		} while(isMatch = matcher.find());
 	}
 
 	public static void main(String[] args) {
@@ -49,8 +39,8 @@ public class Regex {
 			Pattern p = Pattern.compile(args[0]);
 			checkRegex(p, args[1]);
 		} else
-		 // checkRegex(pattern, args[0]);
-		 System.out.println(toDate(args[0]));
+		 checkRegex(TEST_PATTERN, args[0]);
+		 // System.out.println(toDate(args[0]));
 	}
 
 }
