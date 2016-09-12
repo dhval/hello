f4ff985fbcf0828646e94fab978a544656fb79f3
Dhval <dhval@mailinator.com>
2016-09-12 09:20:50 -0400

    resolved merge conflict

--- src/main/java/api/Regex.java
+++ src/main/java/api/Regex.java
@@ -21,6 +21,25 @@ public class Regex {
 	static Pattern PATTERN_DOT_DT = Pattern.compile("^(\\d{4}\\.\\d{2}\\.\\d{2})$");
 
 	static Pattern TEST_PATTERN = Pattern.compile("^t[r|s]p");
+	static String NON_CAPTURING_GROUP = "my (?:cat|dog) pooped"; // [...]  is a character set
+
+	static Pattern patternOrg3 = Pattern.compile("^cn=((business partners|county|federal|state)\\s*(Group\\s*[\\d]+))\\s*?,*?");
+	static Pattern patternUser = Pattern.compile("^cn=([^,]*),*");
+	static Pattern PATTERN_DOT_DT = Pattern.compile("^(\\d{4}\\.\\d{2}\\.\\d{2})$");
+	static Pattern patternG20 = Pattern.compile("^cn=((business partners|county|federal|state)-(\\w*)),ou=*");
+
+	static Pattern pattern = Pattern.compile("^t[r|s]p");
+
+	public static void checkRegex(Pattern pattern, String input) {
+		Matcher matcher = pattern.matcher(input);
+		if (matcher.find()) {
+			System.out.println("Match found ... counts#" + matcher.groupCount() + " --- " + matcher.group(0));
+			for (int i = 1; i <= matcher.groupCount(); i++)
+				System.out.println("Group " + i + " --- " + matcher.group(i));
+		}
+		else
+		System.out.println("... No Match ...");
+	}
 
 	static String toDate(String input) {
 		if (input == null || input.isEmpty())
